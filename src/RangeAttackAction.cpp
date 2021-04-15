7ea74ee929ae2b31fd1e6847031079caab69d97d
Cameron <mcleancam22@gmail.com>
2021-04-15 19:04:26 -0400

    Tiny Merge Conflict Resolved

--- src/RangeAttackAction.cpp
+++ src/RangeAttackAction.cpp
@@ -6,11 +6,11 @@ RangeAttackAction::RangeAttackAction()
 	name = "Attack Action";
 }
 
-RangedAttackAction::~RangedAttackAction()
+RangeAttackAction::~RangeAttackAction()
 = default;
 
 
-void RangedAttackAction::Action(Agent * m_agent)
+void RangeAttackAction::Action(Agent * m_agent)
 {
 	std::cout << "Performing Attack Action" << std::endl;
 }
